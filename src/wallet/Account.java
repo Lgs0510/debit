55f18cb1abefce00de70f9d563f072624e74b430
Luis Gustavo Schabarum <luisgustavo0510@hotmail.com>
2024-10-10 22:54:53 -0300

    CHANGED: Account class implementation

--- src/wallet/Account.java
+++ src/wallet/Account.java
@@ -1,10 +1,56 @@
 package wallet;
 
-public class Account extends Money {
+import java.lang.invoke.MethodHandles;
 
+import org.apache.logging.log4j.*;
+
+public class Account extends Money {
+	private Money balance;
+	private Currency_t currency=Currency_t.BRL;
+	private static Logger AccountLogger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+	
 	public Account(Currency_t currency) {
 		super(0, currency);
+		balance=new Money(0, currency);
+		AccountLogger.info("A new account was created!");
 	}
-	public 
+	
+	public Account(Currency_t currency, Money startBalance) {
+		super(0, currency);
+		balance=new Money(startBalance.getAmount(), currency);
+	}
+	
+	@Override
+	public double getAmount() {return this.balance.getAmount();}
 
+	public void addFunds(Money funds) {
+		if (funds.getAmount() > 0) {
+			if(balance.currencyMatch(funds.getCurrency())) {
+				balance.setAmount(balance.getAmount() + funds.getAmount());
+			}
+			else {
+				throw new IllegalArgumentException("It is not possible to add money from other currencies right now!");
+			}
+		}
+		else {
+			throw new IllegalArgumentException("It is not possible to add negative money!");
+		}
+	}
+	
+	public void withdrawFunds(Money desiredFunds) {
+		if(desiredFunds.getAmount() < 0) {
+			throw new IllegalArgumentException("It is not possible to withdraw negative money!");
+		}
+		
+		if (desiredFunds.getAmount() > balance.getAmount()) {
+			throw new IllegalArgumentException("It is not possible to withdraw more money than what is available!");
+		}
+		
+		if(balance.currencyMatch(desiredFunds.getCurrency())) {
+			throw new IllegalArgumentException("It is not possible to add money from other currencies right now!");
+		}
+			
+		balance.setAmount(balance.getAmount() - desiredFunds.getAmount());
+		
+	}
 }
--- src/wallet/BankAccount.java
+++ src/wallet/BankAccount.java
@@ -1,9 +1,17 @@
 package wallet;
 
-public class BankAccount extends Account {
+import java.lang.invoke.MethodHandles;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BankAccount extends Account {
+	private static Logger bankAccLogger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+	
 	public BankAccount() {
-		// TODO Auto-generated constructor stub
+		super(Currency_t.BRL);
+		
+		bankAccLogger.info("A new bank account was created!");
 	}
 
 }
