4558513b17423a098c0ff8aa018cdddf6d22bfd4
Luis Gustavo Schabarum <luisgustavo0510@hotmail.com>
2024-10-14 21:50:45 -0300

    CHANGED: Account and Money to add JavaDoc comments

--- src/wallet/Account.java
+++ src/wallet/Account.java
@@ -4,11 +4,18 @@ import java.lang.invoke.MethodHandles;
 
 import org.apache.logging.log4j.*;
 
+/**
+ * Account class implements a simple account type, that has money and methods to get, add and withdraw funds
+ */
 public class Account extends Money {
 	private Money balance;
 	private Currency_t currency=Currency_t.BRL;
 	private static Logger AccountLogger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 	
+	/**
+	 * Class constructor specifying the currency type
+	 * @param currency
+	 */
 	public Account(Currency_t currency) {
 		super(0, currency);
 		balance=new Money(0, currency);
@@ -23,6 +30,10 @@ public class Account extends Money {
 	@Override
 	public double getAmount() {return this.balance.getAmount();}
 
+	/**
+	 * Add funds' amount of money to the current account IF currency match and "funds" is greater than zero
+	 * @param funds
+	 */
 	public void addFunds(Money funds) {
 		if (funds.getAmount() > 0) {
 			if(balance.currencyMatch(funds.getCurrency())) {
@@ -37,6 +48,10 @@ public class Account extends Money {
 		}
 	}
 	
+	/**
+	 * Withdraw money from the current account if there is enough for it and the currency matches
+	 * @param desiredFunds
+	 */
 	public void withdrawFunds(Money desiredFunds) {
 		if(desiredFunds.getAmount() < 0) {
 			throw new IllegalArgumentException("It is not possible to withdraw negative money!");
--- src/wallet/Money.java
+++ src/wallet/Money.java
@@ -3,11 +3,19 @@ package wallet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Money class
+ */
 public class Money {
 	private Currency_t currency=Currency_t.BRL;
 	private double amount;
 	private static Logger moneyLogger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 	
+	/**
+	 * Class constructor specifying the amount of money and the currency 
+	 * @param amount
+	 * @param currency
+	 */
 	public Money(double amount, Currency_t currency) {		
 		setAmount(amount);
 		this.currency = currency;
@@ -20,6 +28,10 @@ public class Money {
 	
 	public double getAmount() {return this.amount;}
 	
+	/**
+	 * Change the amount of money. The value must be between 0.01 and 2 billion
+	 * @param amount
+	 */
 	protected void setAmount(double amount) {
 		if(amount < 0.01) {
 			throw new IllegalArgumentException("Amount of monye cannot be lesser then 1 cent ($0.01)");
@@ -30,6 +42,11 @@ public class Money {
 		this.amount=((int)Math.round(amount*100))/100;
 	}
 	
+	/**
+	 * Check if given currency match that of the current money
+	 * @param currencyToCheck
+	 * @return true if matches
+	 */
 	public boolean currencyMatch(Currency_t currencyToCheck) {
 		if(this.getCurrency() == currencyToCheck) {
 			return true;
